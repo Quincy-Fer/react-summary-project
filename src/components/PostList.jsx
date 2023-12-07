54c21fa3b90ca7290c51d2be2143b72275d742dd
Quincy Fer <quincy.e.fer@gmail.com>
2023-12-07 16:50:24 +0100

    Add state to PostList, add event handlers

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,29 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
+import { useState } from "react";
 
 export default function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Quincy" body="I'm awesome" />
-        <Post author="Random" body="you're awesome" />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Random" body="You're awesome" />
       </ul>
     </>
   );
