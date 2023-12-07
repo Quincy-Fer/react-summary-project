30dfef36b4406eb2d423bbef625244b2f2826795
Quincy Fer <quincy.e.fer@gmail.com>
2023-12-07 12:46:39 +0100

    Add NewPost component, add styling

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,15 @@
 import Post from "./Post";
-import classes from "./PostList.module.css"
+import NewPost from "./NewPost";
+import classes from "./PostList.module.css";
 
 export default function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Quincy" body="I'm awesome" />
-      <Post author="Random" body="you're awesome" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Quincy" body="I'm awesome" />
+        <Post author="Random" body="you're awesome" />
+      </ul>
+    </>
   );
 }
