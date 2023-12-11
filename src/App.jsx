a2f19aac2f46f5a153694f1ca5b96c8c63400520
Quincy Fer <quincy.e.fer@gmail.com>
2023-12-11 17:16:35 +0100

    Add state to button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-// import MainHeader from "./components/MainHeader";
+import { useState } from "react";
 import MainHeader from "./components/MainHeader";
 import PostList from "./components/Postlist";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
     <>
-      <MainHeader />
+      <MainHeader onCreatePost={showModalHandler} />
       <main>
-        <PostList />
+        <PostList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
       </main>
     </>
   );
