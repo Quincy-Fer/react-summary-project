7794e9b15d2434793e32413fc9bfa35f51c8ba84
Quincy Fer <quincy.e.fer@gmail.com>
2023-12-13 12:15:03 +0100

    Add styling to icon and note component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,13 @@
 import classes from "./Post.module.css";
+import { TiDeleteOutline } from "react-icons/ti";
 
 export default function Post({ author, body }) {
   return (
     <div className={classes.overflow}>
       <li className={classes.post}>
+        <div className={classes.iconwrapper}>
+          <TiDeleteOutline className={classes.icon}/>
+        </div>
         <p className={classes.author}>{author}</p>
         <p className={classes.text}>{body}</p>
       </li>
